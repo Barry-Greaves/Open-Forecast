c124b13c504617c2d218b2a0465355286ee9d418
Barry-Greaves <musiknonstop909@gmail.com>
2022-12-04 20:08:16 +0000

    add weather image array

--- js/script.js
+++ js/script.js
@@ -11,6 +11,45 @@ let weatherAPIKey = 'eeb6b90e8ef375f3964761f6c685dbf4';
 let weatherBaseEndpoint = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=" + weatherAPIKey;
 let forecastBaseEndpoint = "https://api.openweathermap.org/data/2.5/forecast?units=metric&appid="  +weatherAPIKey;
 
+let weatherImages = [
+  {
+      url: 'images/sunny.png',
+      ids: [800]
+  },
+  {
+      url: 'images/broken-clouds.png',
+      ids: [803, 804]
+  },
+  {
+      url: 'images/few-clouds.png',
+      ids: [801]
+  },
+  {
+      url: 'images/mist.png',
+      ids: [701, 711, 721, 731, 741, 751, 761, 762, 771, 781]
+  },
+  {
+      url: 'images/rain.png',
+      ids: [500, 501, 502, 503, 504]
+  },
+  {
+      url: 'images/scattered-clouds.png',
+      ids: [802]
+  },
+  {
+      url: 'images/shower-rain.png',
+      ids: [520, 521, 522, 531, 300, 301, 302, 310, 311, 312, 313, 314, 321]
+  },
+  {
+      url: 'images/snow.png',
+      ids: [511, 600, 601, 602, 611, 612, 613, 615, 616, 620, 621, 622]
+  },
+  {
+      url: 'images/thnderstorm.png',
+      ids: [200, 201, 202, 210, 211, 212, 221, 230, 231, 232]
+  }
+]
+
 let getWeatherByCityName = async (city) => {
   let endpoint = weatherBaseEndpoint + '&q=' + city;
   let response = await fetch(endpoint);
